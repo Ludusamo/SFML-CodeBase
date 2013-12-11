5d33603a2564db1984cd66987c3d2d5fb32f9ed9
Ludusamo <ludusamo@gmail.com>
2013-12-11 11:58:48 -0500

    Edited Shader. Started Lights.

---
+++ Platformer/include/Light.h
@@ -0,0 +1,14 @@
+#ifndef LIGHT_H
+#define LIGHT_H
+
+
+class Light : public Entity
+{
+    public:
+        Light();
+        ~Light();
+    protected:
+    private:
+};
+
+#endif // LIGHT_H
--- Platformer/res/shaders/AmbientShader.glsl
+++ Platformer/res/shaders/AmbientShader.glsl
@@ -1,13 +1,19 @@
 #define LOWP
 
 uniform sampler2D texture;
+uniform sampler2D lightmap;
 
+uniform vec2 resolution;
 uniform LOWP vec4 ambientColor;
 
 void main() {
 	vec4 diffuseColor = texture2D(texture, gl_TexCoord[0].xy);
-	vec3 ambient = ambientColor.rgb * ambientColor.a;
+	vec2 lighCoord = (gl_FragCoord.xy / resolution.xy);
+	vec4 light = texture2D(lightmap, lighCoord);
 
-	vec3 final = gl_Color.rgb * diffuseColor.rgb * ambient;
-	gl_FragColor = vec4(final, diffuseColor.a);
+	vec3 ambient = ambientColor.rgb * ambientColor.a;
+	vec3 intensity = ambient + light.rgb;
+ 	vec3 finalColor = diffuseColor.rgb * intensity;
+
+	gl_FragColor = gl_Color * vec4(finalColor, diffuseColor.a);
 }
