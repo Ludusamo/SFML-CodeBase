7c2e64c0566ff525accc8d416fb1e73fef9bc804
Alexander Stangl <alesta@bergen.org>
2013-12-11 22:50:29 -0500

    Random Map Generation

--- Platformer/main.cpp
+++ Platformer/main.cpp
@@ -3,8 +3,18 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include "ScreenManager.h"
+#include "TileData.h"
+#include "MapGenerator.h"
 
 int main() {
+    MapGenerator test("res/lvls/1.comp");
+    std::vector<std::vector<int16_t> > testvec = test.generate(10,10, 0.5f);
+    for(int i = 0; i < testvec.size(); i++) {
+        for(int j = 0; j < testvec[0].size(); j++)
+            std::cout << testvec[i][j] << " ";
+        std::cout << std::endl;
+    }
+
     // Window
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT, 32), "SFML Platformer");
 
---
+++ Platformer/src/MapGenerator.cpp
@@ -0,0 +1,184 @@
+#include "MapGenerator.h"
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string>
+#include <vector>
+#include <utility>
+#include <fstream>
+#include <assert.h>
+
+
+MapGenerator::Component::Component(void) : id(NULL), componentMap(std::vector<std::vector<int16_t> >(0, std::vector<int16_t>(0))) {}
+
+MapGenerator::Component::Component(std::string filename) {
+    std::ifstream fin(filename);
+    fin >> this->id;
+    uint32_t width, height;
+    fin >> width >> height;
+    for(int i = 0; i < width; i++) {
+        this->componentMap.push_back(std::vector<int16_t>(height));
+        for(int j = 0; j < height; j++)
+            fin >> this->componentMap[i][j];
+    }
+}
+
+MapGenerator::Component::Component(std::string id, std::vector<std::vector<int16_t> > componentMap) {
+    this->id = id;
+    this->componentMap = componentMap;
+}
+
+MapGenerator::Component::Component(const MapGenerator::Component &other) {
+    this->id = other.id;
+    this->componentMap = other.componentMap;
+}
+
+MapGenerator::Component::~Component(void) {
+
+}
+
+std::string MapGenerator::Component::get_id(void) {
+    return this->id;
+}
+
+std::vector<std::vector<int16_t> > MapGenerator::Component::get_component_map(void) {
+    return this->componentMap;
+}
+
+void MapGenerator::Component::set_id(std::string id) {
+    this->id = id;
+}
+
+void MapGenerator::Component::set_component_map(std::vector<std::vector<int16_t> > componentMap) {
+    this->componentMap = componentMap;
+}
+
+void MapGenerator::Component::populate(std::string filename) {
+    std::ifstream fin(filename);
+    fin >> this->id;
+    uint32_t width, height;
+    fin >> width >> height;
+    for(int i = 0; i < width; i++) {
+        this->componentMap.push_back(std::vector<int16_t>(height));
+        for(int j = 0; j < height; j++)
+            fin >> this->componentMap[i][j];
+    }
+}
+
+void MapGenerator::Component::populate(std::string id, std::vector<std::vector<int16_t> > componentMap) {
+    this->id = id;
+    this->componentMap = componentMap;
+}
+
+void MapGenerator::Component::populate(const MapGenerator::Component other) {
+    this->id = other.id;
+    this->componentMap = other.componentMap;
+}
+
+MapGenerator::Component& MapGenerator::Component::operator=(const MapGenerator::Component other) {
+    this->id = other.id;
+    this->componentMap = other.componentMap;
+    return *this;
+}
+
+void MapGenerator::Component::paint_to(std::vector<std::vector<int16_t> > &generation) {
+    uint32_t xpos = std::rand() % generation.size();
+    uint32_t ypos = std::rand() % generation[0].size();
+    std::cout << xpos << " " << ypos << std::endl;
+    for(uint32_t x = xpos; x < xpos + this->componentMap.size(); x++) {
+        if(x >= generation.size())
+            break;
+        for(uint32_t y = ypos; y < ypos + this->componentMap[0].size(); y++) {
+            if(y >= generation[0].size())
+                break;
+            if(this->componentMap[x - xpos][y - ypos] != -1)
+                generation[x][y] = this->componentMap[x - xpos][y - ypos];
+        }
+    }
+}
+
+void MapGenerator::Component::paint_to(uint32_t xpos, uint32_t ypos, std::vector<std::vector<int16_t> > &generation) {
+    for(uint32_t x = xpos; x < generation.size(); x++) {
+        for(uint32_t y = ypos; y < generation[0].size(); y++) {
+            if(this->componentMap[x][y] == -1)
+                continue;
+            generation[x][y] = this->componentMap[x][y];
+        }
+    }
+}
+
+MapGenerator::MapGenerator(void) : walls(true), resources(std::vector<MapGenerator::Component>(0)), generation(std::vector<std::vector<int16_t> >(0, std::vector<int16_t>(0))) {}
+
+MapGenerator::MapGenerator(std::string file) : walls(true), generation(std::vector<std::vector<int16_t> >(0, std::vector<int16_t>(0))) {
+    this->resources.push_back(MapGenerator::Component(file));
+}
+
+MapGenerator::MapGenerator(std::vector<std::string> files) : walls(true), generation(std::vector<std::vector<int16_t> >(0, std::vector<int16_t>(0))){
+    for(int i = 0; i < files.size(); i++)
+        this->resources.push_back(MapGenerator::Component(files[i]));
+}
+
+MapGenerator::MapGenerator(const MapGenerator &other) : walls(other.walls), resources(other.resources), generation(std::vector<std::vector<int16_t> >(0, std::vector<int16_t>(0))) {}
+
+MapGenerator::~MapGenerator(void) {
+
+}
+
+void MapGenerator::fetch_resources(std::string file) {
+    this->resources.clear();
+    this->resources.push_back(MapGenerator::Component(file));
+}
+
+void MapGenerator::fetch_resources(std::vector<std::string> files) {
+    this->resources.clear();
+    for(int i = 0; i < files.size(); i++)
+        this->resources.push_back(MapGenerator::Component(files[i]));
+}
+
+void MapGenerator::append_resources(std::string file) {
+    this->resources.push_back(MapGenerator::Component(file));
+}
+
+void MapGenerator::append_resources(std::vector<std::string> files) {
+    for(int i = 0; i < files.size(); i++)
+        this->resources.push_back(MapGenerator::Component(files[i]));
+}
+
+void MapGenerator::remove_resources(uint32_t index) {
+    this->resources.erase(this->resources.begin() + index);
+}
+
+void MapGenerator::remove_resources(std::string id) {
+    for(int i = 0; i < this->resources.size(); i++)
+        if(this->resources[i].get_id() == id)
+            this->resources.erase(this->resources.begin() + i);
+}
+
+void MapGenerator::clear_resources(void) {
+    this->resources.clear();
+}
+
+uint32_t MapGenerator::get_solids(void) {
+    uint32_t count = 0;
+    for(uint32_t i = 0; i < this->generation.size(); i++)
+        for(uint32_t j = 0; j < this->generation[0].size(); j++)
+            count += generation[i][j] == 1 ? 1 : 0;
+    return count;
+}
+
+std::vector<std::vector<int16_t> > MapGenerator::generate(uint32_t width, uint32_t height, float weight) {
+    assert(weight < 1 && weight >= 0);
+    for(int i = 0; i < width; i++)
+        this->generation.push_back(std::vector<int16_t>(height, 1));
+    while((float) this->get_solids() / (width * height) > weight) {
+        uint32_t index = std::rand() % resources.size();
+        this->resources[index].paint_to(this->generation);
+    }
+    return this->generation;
+}
+
+std::vector<std::vector<int16_t> > MapGenerator::generate(uint32_t width, uint32_t height, std::vector<float> weights) {
+    for(int i = 0; i < width; i++)
+        this->generation.push_back(std::vector<int16_t>(height, 1));
+}
